cbfab21db11a011ce3e1423d2b0c63617a9f377e
dkzeanah <dkzeanah@gmail.com>
2022-10-05 06:02:24 +0900

    MSSA Root Folder Init

---
+++ day2/Program.cs
@@ -0,0 +1,82 @@
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+// // See https://aka.ms/new-console-template for more information
+
+// Console.WriteLine("Hello mate!");
+
+// // course duration, days/weeks, course grade, credit hours, location
+// int ConcurrentExclusiveSchedulerPair = 101;
+// int lengthInWeeks = 10;
+// double myGPA = 0.0;
+
+
+// //char
+
+// string courseTitle = "CS101";
+// string courseDescription = "This course teaches introductory programming topics";
+// string instructorName = "Brian Gorman";
+
+
+
+// bool doesExist = true;
+// bool passingGrade = true;
+// bool isEnrolled = false;
+
+
+// //const
+
+// const int numberOfWeeks_ThreeHour = 10;
+// const int numberOfWeeks_SixHour = 5;
+
+
+// Console.WriteLine(numberOfWeeks_ThreeHour);
+
+
+//
+
+
+// ## ask the user what they want to do
+
+//use integers for menu options 
+
+//TODO #2: Get the user response
+
+var inputl = Convert.ToInt32(Console.ReadLine());
+var input2 = Console.ReadLine();
+var choice = Convert.ToInt32(input2);
+//TODO #3: Based on user response, perform an operation
+
+
+
+Console.WriteLine("What would you like to do? [Add = 1, Sub = 2, Mult = 3, Div =4]");
+var input = (Console.ReadLine();
+var input = (int)Console.ReadLine();
+var input2 = Console.ReadKey();
+
+
+if (input11 == 1)
+{
+    Console.WriteLine("Do the add here");
+
+}
+else
+{
+    if |Cinput1 == 2)
+
+}
+//Sub
+Console.WriteLine("Do the subtract here");
+
+else if (inputl == 3)
+{
+//Multiply
+Console.WriteLine("Do the subtract here");
+}
+else if (input1 == 4)
+{
+
+//Divide
+Console.WriteLine("Do the subtract here");
+  
+}
